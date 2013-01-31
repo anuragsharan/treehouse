b76aca0807522694208d171765947eb6dd1e0846
Anurag Sharan <anuragsharan@gmail.com>
2013-01-31 23:36:56 +0000

    this is my first git and ruby on rail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treehouse::Application.config.secret_token = '420cb228cc447a3736e6383285f5ec426ffab41e323394d22f002a2bd15de885149eb868c17ab1405fc741c8aacfd702945a8d56f7ce9675e1592002fa763500'
